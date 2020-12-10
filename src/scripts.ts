0b0606c697607ba110526506e7ddf908b6b55ed8
Aaron Powell <Apowell829@gmail.com>
2020-12-10 22:00:13 +0000

    show all working cameras, block spamming start camera

--- src/scripts.ts
+++ src/scripts.ts
@@ -1,9 +1,9 @@
-import { getFriendlyErrorMessage } from './errorMessages';
+import { getFriendlyErrorMessage } from "./errorMessages";
 
-const videoHolder: HTMLDivElement = document.querySelector('.video');
-const text: HTMLParagraphElement = document.querySelector('.text');
-const startbutton: HTMLButtonElement = document.querySelector('.start-camera');
-
+const videoHolder: HTMLDivElement = document.querySelector(".video");
+const text: HTMLParagraphElement = document.querySelector(".text");
+const startbutton: HTMLButtonElement = document.querySelector(".start-camera");
+let requesting = false;
 function handleError(err: MediaStreamError) {
   text.textContent = getFriendlyErrorMessage(err);
 }
@@ -11,8 +11,8 @@ function handleError(err: MediaStreamError) {
 async function getCameras() {
   const devices = await navigator.mediaDevices.enumerateDevices();
   const cameras = devices
-    .filter((device) => device.kind === 'videoinput')
-    .filter((device) => !device.label.includes('Camo'));
+    .filter((device) => device.kind === "videoinput")
+    .filter((device) => !device.label.includes("Camo"));
   // camera 2
   return cameras;
 }
@@ -29,15 +29,14 @@ function createVideoElementFromCamera(camera: MediaDeviceInfo) {
 }
 
 async function requestIntialAccess() {
+  // prevent spamming start camera, wait for request to finish
+  if (requesting) return;
   // clear out old cameras
-  videoHolder.innerHTML = '';
-  console.log(navigator);
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: false,
-    video: true,
-  });
-  console.log('initial stream');
+  requesting = true;
+  videoHolder.innerHTML = "";
+
   const cameras = await getCameras();
+
   // See how many streams we are allowed access to
   const streamPromises = cameras.map(function (camera) {
     return navigator.mediaDevices.getUserMedia({
@@ -45,9 +44,17 @@ async function requestIntialAccess() {
       video: { deviceId: { exact: camera.deviceId } },
     });
   });
-  // wait for access to ALL the streams
-  const streams = await Promise.all(streamPromises).catch(console.error);
-  console.log({ streams });
+
+  // Still show cameras even if one isn't working.
+  const streams: PromiseSettledResult<MediaStream>[] = await Promise.allSettled(
+    streamPromises
+  );
+  const workingStreams: MediaStream[] = streams.reduce((acc, current) => {
+    if (current.status === "fulfilled") {
+      acc = [...acc, current.value];
+    }
+    return acc;
+  }, []);
 
   // Create video elements for each
   const videoFragment: DocumentFragment[] = cameras.map(
@@ -57,12 +64,15 @@ async function requestIntialAccess() {
   videoFragment.forEach((el) => videoHolder.append(el));
 
   // Assign the src media of each camera to the video elements
-  const videoElements = videoHolder.querySelectorAll('video');
+  const videoElements = videoHolder.querySelectorAll("video");
   videoElements.forEach((el, i) => {
-    el.srcObject = streams[i];
+    if (workingStreams) {
+      el.srcObject = workingStreams[i];
+    }
   });
+  requesting = false;
 }
 
-startbutton.addEventListener('click', requestIntialAccess);
+startbutton.addEventListener("click", requestIntialAccess);
 
 requestIntialAccess();
